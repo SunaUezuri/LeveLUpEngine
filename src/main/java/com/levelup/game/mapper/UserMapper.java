9f35d0145eed7fe5e7c05697deb2daeb04b45009
SunaUezuri <wesley.jane.santos@gmail.com>
2025-11-20 17:20:11 -0300

    feat: Criando o Mappper para usuário

---
+++ src/main/java/com/levelup/game/mapper/UserMapper.java
@@ -0,0 +1,33 @@
+package com.levelup.game.mapper;
+
+import com.levelup.game.dto.user.UserResponseDto;
+import com.levelup.game.dto.user.UserSelectDto;
+import com.levelup.game.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public UserResponseDto toDto(User entity) {
+        if (entity == null) return null;
+
+        return new UserResponseDto(
+                entity.getId(),
+                entity.getFullName(),
+                entity.getEmail(),
+                entity.getJobTitle(),
+                (entity.getTeam() != null) ? entity.getTeam().getTeamName() : "Sem Time",
+                entity.getRole(),
+                entity.getIsActive()
+        );
+    }
+
+    public UserSelectDto toSelectDto(User entity) {
+        if (entity == null) return null;
+
+        return new UserSelectDto(
+                entity.getId(),
+                entity.getFullName()
+        );
+    }
+}
