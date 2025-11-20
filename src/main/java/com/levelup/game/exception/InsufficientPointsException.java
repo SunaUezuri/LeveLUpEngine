759888e63ecb66133cfda605242e96b566e548dc
SunaUezuri <wesley.jane.santos@gmail.com>
2025-11-20 17:45:06 -0300

    feat: Criando uma classe de erro para pontos insuficientes

---
+++ src/main/java/com/levelup/game/exception/InsufficientPointsException.java
@@ -0,0 +1,7 @@
+package com.levelup.game.exception;
+
+public class InsufficientPointsException extends BusinessRuleException {
+    public InsufficientPointsException(String message) {
+        super(message);
+    }
+}
