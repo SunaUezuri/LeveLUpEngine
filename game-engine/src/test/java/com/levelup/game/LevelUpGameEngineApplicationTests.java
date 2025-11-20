019701875b9f41b5fe436e225aa2d803d005a6d1
SunaUezuri <wesley.jane.santos@gmail.com>
2025-11-20 15:20:16 -0300

    feat: Criando a base do projeto no Initialzr

---
+++ game-engine/src/test/java/com/levelup/game/LevelUpGameEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelup.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelUpGameEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
